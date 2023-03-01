eb053b324fbd37b002a05c74069207f25872f33e
kdan2944 <kdan2944gmail.com>
2023-03-01 09:27:53 +0300

    update

--- src/components/books/bookCatagories.js
+++ src/components/books/bookCatagories.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import './book.css';
 
 function Catagories() {
+  const { categories } = useSelector((state) => state.categories);
+  const [searchField, setSearchField] = useState('');
+  const [filterResult, setFilterResult] = useState(categories);
+
+  const filterHandler = () => {
+    setFilterResult(filterResult.map((item) => item.country.includes(searchField)));
+  };
   return (
     <div>
-      <button type="button">check status</button>
+      <input type="text" value={searchField} onChange={(e) => setSearchField(e.target.value)} />
+      <button type="button" onClick={filterHandler}>Filter</button>
     </div>
   );
 }
