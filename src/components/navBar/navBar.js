3f613beef0e478e21195fe8fd59ff284fcfca15f
kdan2944 <kdan2944gmail.com>
2023-03-04 20:52:18 +0300

    add styling

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -9,7 +9,7 @@ function Nav() {
       <ul className="nav-links">
         <li><Link className="logo" to="/">BookStore CMS</Link></li>
         <li><Link className="links" to="/">BOOKS</Link></li>
-        <li><Link className="links" to="/categories">CATEGORIES</Link></li>
+        <li><Link className="links-cate" to="/categories">CATEGORIES</Link></li>
       </ul>
       <ul className="icon">
         <li><Link className="user" to="/user"><FaUserCircle /></Link></li>
